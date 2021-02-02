bff3445d7b94411e5581a683b07e2210edbed883
chalosalvador <chalosalvador@gmail.com>
2021-02-02 10:03:38 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function formatName( user ) {
+  return `${ user.name } ${ user.lastname }`;
+}
+
+// DRY => Dont Repeat Yourself
+function App( { users } ) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Lista de usuarios</h1>
+      <ul>
+      {
+        users.map( ( user ) => {
+          return <li>Hola { formatName( user ) }</li>;
+        } )
+      }
+      </ul>
+    </>
+  )
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const users = [
+  {
+    name: 'Chalo',
+    lastname: 'Salvador Pesantes'
+  },
+  {
+    name: 'María',
+    lastname: 'Chavez'
+  },
+  {
+    name: 'Carlos',
+    lastname: 'Perez'
+  }
+];
+
+ReactDOM.render( <App users={ users } />,
+  document.getElementById( 'root' )
 );
 
 // If you want to start measuring performance in your app, pass a function
